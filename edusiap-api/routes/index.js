044f605db09a26ab526e01178492e15034799a5a
ichsan-frds <bsmbangirfan@gmail.com>
2025-04-19 12:58:22 +0700

    feat: added api endpoint for quiz and storybook

--- edusiap-api/routes/index.js
+++ edusiap-api/routes/index.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const userRoutes = require('./UserRoutes');
 const videoRoutes = require('./VideoRoutes');
+const storybookRoutes = require('./StorybookRoutes');
+const quizRoutes = require('./QuizRoutes')
 
 router.use('/users', userRoutes);
 router.use('/videos', videoRoutes);
+router.use('/storybook', storybookRoutes);
+router.use('/quiz', quizRoutes);
 
 module.exports = router;
