4b0c09586cbd5335f447682aab58a0e3a1ffa24a
muhammad22015 <muhammad22015@mail.unpad.ac.id>
2025-06-07 09:23:21 +0700

    Ubah port server.js ke 8080 untuk Cloud Run

--- edusiap-api/server.js
+++ edusiap-api/server.js
@@ -13,7 +13,7 @@ app.get('/', (req,res) => {
 
 app.use('/', router);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
